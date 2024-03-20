6d9c0bc61ce8b14383faa13f2b4deab60f0cae00
sunwane <auxuanhoa@gmail.com>
2024-03-20 22:04:05 +0700

    up file

--- QuanTriNhanSu/QuanTriNhanSu.cpp
+++ QuanTriNhanSu/QuanTriNhanSu.cpp
@@ -1,4 +1,4 @@
-// QuanTriNhanSu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// QuanTriNhanSu.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
-    cout << " Toi ghet code mhuhuuhu";
+    cout << " Toi ghet code mhuhuuhu\n";
+    cout << "ĐCM GITHUB ĂN MẶN ĐÁI KHAI HỌC LỚP 2 KHAI LỚP 9";
 }
 
