d117cc552a285b2b16f1d7b92659ad9cde1bef9f
Hiuth <Hiuth2004@gmail.com>
2024-03-27 21:07:33 +0700

    up file node cpp

--- QuanTriNhanSu/Node.h
+++ QuanTriNhanSu/Node.h
@@ -1,5 +1,23 @@
 #pragma once
 class Node
 {
+private:
+	int key;
+	Node* left;
+	Node* right;
+	Node* parent;
+public:
+	Node();
+	Node(int);
+	virtual ~Node();
+	Node* GetLeft() { return left; }
+	Node* GetRight() { return right; }
+	void SetLeft(Node* val) { left = val; }
+	void SetRight(Node* val) { right = val; }
+	Node* GetParent() { return parent; }
+	void SetParent(Node* val) { parent = val; }
+	int GetKey() { return key; }
+	void SetKey(int val) { key = val; }
+protected:
 };
 
