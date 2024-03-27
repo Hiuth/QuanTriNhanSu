22e580e16d397f5ae03cc9facb6a33c23685ac0e
Hiuth <Hiuth2004@gmail.com>
2024-03-27 21:47:06 +0700

    update file Node .cpp

--- QuanTriNhanSu/Node.cpp
+++ QuanTriNhanSu/Node.cpp
@@ -1,14 +1,21 @@
 #include "Node.h"
 #include <iostream>
 Node::Node() {
-	this->key = 0;
+	this->TenTaiKhoan = 0;
+	this->QuanLyNhanSu = 0;
+	this->QuanLyTienLuong = 0;
+	this->MatKhau = 0;
 	this->left = NULL;
 	this->right = NULL;
 	this->parent = NULL;
 }
 
-Node::Node(int x) {
-	this->key = x;
+Node::Node(long TenTk, int MatKhau,int admin, int HRM, int FM) {//financial management
+	this->TenTaiKhoan = TenTk;
+	this->MatKhau = MatKhau;
+	this->Admin = admin;
+	this->QuanLyNhanSu = HRM;
+	this->QuanLyTienLuong = FM;
 	this->left = NULL;
 	this->right = NULL;
 	this->parent = NULL;
