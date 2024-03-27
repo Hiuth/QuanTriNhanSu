34eb8ab7ad1aa39885c1477e1164ef29be737541
Hiuth <Hiuth2004@gmail.com>
2024-03-27 21:35:27 +0700

    up file he thong .h

--- QuanTriNhanSu/HeThong.h
+++ QuanTriNhanSu/HeThong.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "Node.h"
 class HeThong
 {
+private:
+	Node* root;
+public:
+	HeThong();
+	virtual ~HeThong();
+	Node* GetRoot() { return root; }
+	void SetRoot(Node* val) { root = val; }
+	bool InsertNode(Node*);
+	bool deleteNode(Node*);
+	void PrintAccount();
 };
 
