5cba1ab5b1ba5e177cadbbf6df91c793addd834b
Hiuth <Hiuth2004@gmail.com>
2024-03-28 14:56:36 +0700

    HeThong .h

--- QuanTriNhanSu/HeThong.h
+++ QuanTriNhanSu/HeThong.h
@@ -3,18 +3,21 @@
 class HeThong
 {
 private:
-	Node* root;
+	Node* head;
+	Node* tail;
 public:
 	HeThong();
 	virtual ~HeThong();
-	Node* GetRoot() { return root; }
-	void SetRoot(Node* val) { root = val; }
+	Node* GetHead() { return head; }
+	void SetHead(Node* val) { head = val; }
+	Node* GetTail() { return tail; }
+	void SetTail(Node* val) { tail = val; }
 	bool InsertNode(Node*);
 	bool deleteAccount(Node*);//deleteNode;
 	void PrintAccount();
 	void CheckAccount(Node*);
 	void EditAccount(Node*);
-	void CreateAccount();
+	void CreateAccount(Node*);
 
 
 };
