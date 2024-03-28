4d3a0bf03cfa7c97d5b4e4135e2722f0a85be709
Hiuth <Hiuth2004@gmail.com>
2024-03-28 14:54:49 +0700

    update Node

--- QuanTriNhanSu/Node.h
+++ QuanTriNhanSu/Node.h
@@ -8,19 +8,13 @@ private:
 	int Admin;
 	int QuanLyNhanSu;
 	int QuanLyTienLuong;
-	Node* left;
-	Node* right;
-	Node* parent;
+	Node* pointer;
 public:
 	Node();
 	Node(long, int, int, int, int);
 	virtual ~Node();
-	Node* GetLeft() { return left; }
-	Node* GetRight() { return right; }
-	void SetLeft(Node* val) { left = val; }
-	void SetRight(Node* val) { right = val; }
-	Node* GetParent() { return parent; }
-	void SetParent(Node* val) { parent = val; }
+	Node* GetPointer() { return pointer; }
+	void SetPointer(Node* val) { pointer = val; }
 	long GetAccountName() { return TenTaiKhoan; }
 	void SetAccountName(long val) { TenTaiKhoan = val; }
 	int GetPassword() { return MatKhau; }
