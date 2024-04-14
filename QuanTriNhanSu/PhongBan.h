76175b97bc9c56df3a39ebccaa136a4f98353067
Hiuth <Hiuth2004@gmail.com>
2024-04-14 21:17:55 +0700

    update

--- QuanTriNhanSu/PhongBan.h
+++ QuanTriNhanSu/PhongBan.h
@@ -1,6 +1,6 @@
 ﻿#include "KetNoi.h"
 #include "NodePhongBan.h"
-
+#include<vector>
 
 class PhongBan {
 private:
@@ -12,9 +12,11 @@ public:
     void InputDepartment();
     void InputEditDepartment();
     void InputSearchDepartment();
+    void InputDeleteDepartment();
     void CreateDepartment(NodePhongBan* p); // Tạo mới phòng ban
-    void PrintDepartmentInfotmation(); // In thông tin phòng ban
-    void SearchDepartment(string ten, string ma); // Tìm kiếm phòng ban
+    void PrintDepartmentInfotmation(vector<NodePhongBan>); // In thông tin phòng ban
+    vector<NodePhongBan> SearchDepartment(string ten, string ma); // Tìm kiếm phòng ban
+    vector<NodePhongBan> TakeAllDepartment();
     void DeleteDepartment(string xoa); // Xóa phòng ban
     void EditDepartment(string ChoCanSua, string MuonDoiThanh, string TenPB); // Sửa thông tin phòng ban
 };
